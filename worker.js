6fc58f038cd43c2ffd4090d3d136e557e5256334
kuspia <63403330+kuspia@users.noreply.github.com>
2022-05-17 18:04:54 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -4,8 +4,6 @@ self.addEventListener("push", e => {
   self.registration.showNotification(data.title, {
     body: "Provide the way to AMBULANCE ♥ ",
     vibrate: [200, 100, 200, 100, 200, 100, 200],
-    timeout: 10000,
-
     icon: "https://raw.githubusercontent.com/NON-Localhost/Ambulance-Notifier/main/192.png",
     badge: "https://raw.githubusercontent.com/NON-Localhost/Ambulance-Notifier/main/96.png"
 
