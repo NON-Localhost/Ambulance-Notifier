3259355160ac862c1da18c5827f094e4bd74ec88
kuspia <63403330+kuspia@users.noreply.github.com>
2022-05-18 12:13:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ webpush.setVapidDetails("mailto:test@test.com",publicVapidKey,privateVapidKey);
 var path = require('path');
 app.use(express.static(path.join(__dirname, '' ))); 
 
- 
+  
 var mongoose = require('mongoose');
+const { getMaxListeners } = require('process');
 // mongoose.connect('mongodb://localhost:27017/ambu', {useNewUrlParser: true , useUnifiedTopology: true});
 // app.use(cors);
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.a7rq8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
@@ -32,8 +33,8 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS
 // console.log("Monkeyyyyyyyy connected man !!")
 // });
 
-
-const forma = new mongoose.Schema({
+// this is for userlive and ambulive
+const forma = new mongoose.Schema({ 
     _id: String,
     type: String,
     lat: Number,
@@ -43,10 +44,17 @@ const forma = new mongoose.Schema({
     { timestamps: true }
 
 );
+// this is for blocked
+const forma2 = new mongoose.Schema({
+    _id: String
+},
+{ timestamps: true }
+);
 
 
 const ambulive = mongoose.model('ambulive', forma); 
 const userlive = mongoose.model('userlive', forma); 
+const blocked = mongoose.model('blocked', forma2); 
 
 app.use(express.json());
 app.get('/', (req, res) => {   
@@ -56,15 +64,50 @@ app.get('/', (req, res) => {
 
 var subscription;
 var payload;
-
 app.post("/sub", (req, res) => {
      subscription = req.body;
     res.status(201).json({});
      payload = JSON.stringify({ title: "Ambulance Alert!!" });
     
-  }); 
+}); 
 
-app.post('/', async (req, res) => {
+app.post('/', async (req, res) => { 
+    // this code can also be executed over some other server to optimize the performance
+
+    // let docs2 = await blocked.find({}).lean();
+    // blkarr = docs2.filter((doc) => doc !== null);
+    // const freearr = new Array();
+    // for (i = 0; i < blkarr.length; i++)
+    // {
+    //     var tx = blkarr[i].createdAt;
+    //     var ty = new Date();
+    //     var diffs = (ty-tx)/1000;
+    //     var diffh = (diffs)/3600;
+    //     var stay_up = 0.005;  ////////////////////////////////// Important variable to destroy the blocking criteria
+    //    console.log( diffh );
+    //    if(diffh > stay_up)
+    //    {
+    //        freearr.push(blkarr[i]._id);
+    //    }
+
+    // }
+
+    // for (i = 0; i < freearr.length; i++)
+    // {
+    //     var destroyme = freearr[i];
+    //    await blocked.findByIdAndDelete(destroyme, function (err, docs) {
+    //         if (err){ 
+    //             console.log(err);
+    //         } 
+    //         else{
+    //             console.log("Deleted : ", docs);
+    //         }
+    //     });
+        
+    // }
+
+    ///////////////// instead of scanning whole just scan for the frontend
+    /////////////////////// email and then u can do that
 
     const { lat, lon, eid, utype } = req.body;
     const { authorization } = req.headers;
@@ -133,17 +176,7 @@ app.post('/', async (req, res) => {
 
     // n^2 complexity to check every ambulive loc with every userlive loc and then send the notification 
 
-    async function getRecords() {
-        let docs = await userlive.find({}).lean();
-        userarr = docs.filter((doc) => doc !== null); 
-        return userarr;
-    }
-    async function getRecords1() {
-        let docs = await ambulive.find({}).lean();
-        ambuarr = docs.filter((doc) => doc !== null); 
-        return ambuarr;
-    }
-    
+
   
     let rnge = 5 // radious (km) of circle for alert region 
 
@@ -153,26 +186,52 @@ app.post('/', async (req, res) => {
     userarr = docs1.filter((doc) => doc !== null); 
 
     //console.log( ambuarr.length , userarr.length)
+    
 
     for (i = 0; i < ambuarr.length; i++) {
         let ax = ambuarr[i].lat;
         let ay = ambuarr[i].lon;
        // console.log("For ambulance" , i+1)
-        for (j = 0; j <  userarr.length; j++) {
+        for (j = 0; j <  userarr.length; j++) 
+        {
             let ux = userarr[j].lat;
             let uy = userarr[j].lon;
 
             let ans = dis ( ax,ux,ay,uy)
-       // console.log( "Ambulance driver" , ambuarr[i]._id , "is far away by" , ans , "kms", "with user" , userarr[j]._id )
+          // console.log( "Ambulance driver" , ambuarr[i]._id , "is far away by" , ans , "kms", "with user" , userarr[j]._id );
+
             if( ans < rnge)
             {
-            console.log( "Ambulance driver" , ambuarr[i]._id , "is far away by" , ans , "kms", "with user" , userarr[j]._id )
-            
-            webpush.sendNotification(subscription, payload).catch(err => console.error(err));
-    
-        }
+
+                const user232 = await blocked.find({_id: eid});
+                
+                if(user232.length != 1 ) // it is not blocked 
+                {
+             console.log( "Ambulance driver" , ambuarr[i]._id , "is far away by" , ans , "kms", "with user" , userarr[j]._id )
+          
+              
+             webpush
+             .sendNotification(subscription, payload)
+             .catch(err => console.error(err));
+             webpush
+             .sendNotification(subscription, payload)
+             .catch(err => console.error(err));
+           
+             
+            const newUser21342 = await blocked.create({_id: eid }); 
+               }
+               else
+               {
+
+                console.log( eid , "is blocked and will be release after 1 hour");
+
+               }
+                
+        
+        
+            }
 }
-}
+    }
     res.send({
         lat,
         lon,
@@ -182,13 +241,9 @@ app.post('/', async (req, res) => {
 
 });
 
-
-
 function dis(lat1,lat2, lon1, lon2){lon1 = lon1 * Math.PI / 180;lon2 = lon2 * Math.PI / 180;lat1 = lat1 * Math.PI / 180;lat2 = lat2 * Math.PI / 180;let dlon = lon2 - lon1;let dlat = lat2 - lat1;let a = Math.pow(Math.sin(dlat / 2), 2)+ Math.cos(lat1) * Math.cos(lat2)* Math.pow(Math.sin(dlon / 2),2);let c = 2 * Math.asin(Math.sqrt(a));let r = 6371;return(c * r);}
-
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
 
-////////////////////////////////
